797fc46e9ed84b137f94dd49148a08bf4c9305f8
Tasneem <thalim.glam@gmail.com>
2024-02-02 12:37:27 -0800

    Added user controller inside controller folder

---
+++ controller/user-controllers.js
@@ -0,0 +1,66 @@
+const { User, Thought } = require("../models");
+
+const userController = {
+  getAllUser(req,res){
+    User.find({})
+      .populate({
+        path: "friends",
+        select: "__v",
+      })
+      .select("-__v")
+      .sort({_id: -1 })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  getUserById({ params }, res){
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts",
+        select: "__v",
+      })
+      .populate({
+        path: "friends",
+        select: "__v",
+      })
+      .select("-__v")
+      .then((dbUserData) => {
+        if(!dbUserData) {
+          return res.status(404).json({ message: " No such user found "});
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+    createUser({ body }, res){
+      User.create(body)
+          .then((dbUserData) => res.json(dbUserData))
+          .catch((err) => res.json(err));
+  },
+
+    updateUser({ params, body }, res) {
+      User.findOneAndUpdate({_id: params.id }, body, {
+        new: true,
+        rinValidators: true,
+      })
+      .then((dbUserData) => {
+        if(!dbUserData) {
+          res.status(404).json({ message: " No such user found "});
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+    },
+
+    deleteUser
+
+
+}
