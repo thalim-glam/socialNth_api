d3c804ff0fa69b2195f30703f4ec552ad1800cf5
Tasneem <thalim.glam@gmail.com>
2024-02-02 13:27:15 -0800

    Updating user controller and trying to do the bonus part

--- controller/user-controllers.js
+++ controller/user-controllers.js
@@ -1,14 +1,14 @@
 const { User, Thought } = require("../models");
 
 const userController = {
-  getAllUser(req,res){
+  getAllUser(req, res) {
     User.find({})
       .populate({
         path: "friends",
         select: "__v",
       })
       .select("-__v")
-      .sort({_id: -1 })
+      .sort({ _id: -1 })
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => {
         console.log(err);
@@ -16,7 +16,7 @@ const userController = {
       });
   },
 
-  getUserById({ params }, res){
+  getUserById({ params }, res) {
     User.findOne({ _id: params.id })
       .populate({
         path: "thoughts",
@@ -28,8 +28,8 @@ const userController = {
       })
       .select("-__v")
       .then((dbUserData) => {
-        if(!dbUserData) {
-          return res.status(404).json({ message: " No such user found "});
+        if (!dbUserData) {
+          return res.status(404).json({ message: " No such user found " });
         }
         res.json(dbUserData);
       })
@@ -39,28 +39,78 @@ const userController = {
       });
   },
 
-    createUser({ body }, res){
-      User.create(body)
-          .then((dbUserData) => res.json(dbUserData))
-          .catch((err) => res.json(err));
+  // Create User
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
   },
-
-    updateUser({ params, body }, res) {
-      User.findOneAndUpdate({_id: params.id }, body, {
-        new: true,
-        rinValidators: true,
-      })
+  // Update user
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      rinValidators: true,
+    })
       .then((dbUserData) => {
-        if(!dbUserData) {
-          res.status(404).json({ message: " No such user found "});
+        if (!dbUserData) {
+          res.status(404).json({ message: " No such user found " });
           return;
         }
         res.json(dbUserData);
       })
       .catch((err) => res.json(err));
-    },
+  },
 
-    deleteUser
+  // Delete User
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: " No such user found " });
+          return;
+        }
+        return Thought.deleteMany({ _id: { $in: dbUserData.thoughts } });
+      })
+      .then(() => {
+        res.json({ message: "User and user_thought being deleted! " });
+      })
+      .catch((err) => res.json(err));
+  },
 
+  //-----------------------------------------------------------
+
+  addFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $addToSet: { friends: params.friendId } },
+      { new: true, runValidators: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: " No such user found " });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  //-------------------------------------------------------------------
+  removeFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: " No such user found " });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
 
 }
+ module.exports = userController ;
