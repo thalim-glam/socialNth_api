2de5922f513fc86da1545e038c44c95919a6e36c
Tasneem <thalim.glam@gmail.com>
2024-02-03 21:23:46 -0800

    Creating package json

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
