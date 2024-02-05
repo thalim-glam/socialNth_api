94f950de5946737dfcffbb541f44673437f2b219
Tasneem <thalim.glam@gmail.com>
2024-02-05 12:13:11 -0800

    Trying to fix bug

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
-
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
